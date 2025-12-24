ace5c90d479cbc7b060d7b6d77c95b4b17df1fd4
Salsabil <Salsabilelshiekh6@gmail.com>
2025-12-24 17:03:21 +0200

    feat: enhance DebtsInReports widget to handle loading state and display dynamic debt values

--- lib/features/reports/presentation/views/widgets/debts_in_reports.dart
+++ lib/features/reports/presentation/views/widgets/debts_in_reports.dart
@@ -1,12 +1,48 @@
 import 'package:alalamia/core/helper/translation_extensions.dart';
+import 'package:alalamia/features/reports/data/models/reports_model.dart';
+import 'package:alalamia/features/reports/presentation/cubit/reports_cubit.dart';
+import 'package:alalamia/features/reports/presentation/cubit/reports_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
+import '../../../../../core/enums/request_status.dart';
 import '../../../../../core/helper/app_extention.dart';
 import '../../../../../core/helper/number_extentions.dart';
 
 class DebtsInReports extends StatelessWidget {
   const DebtsInReports({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ReportsCubit, ReportsState>(
+      builder: (context, state) {
+        bool isLoading = state.requestStatus.isLoading;
+        return Skeletonizer(
+          enabled: isLoading,
+          child: DebtItemInReport(
+            model: isLoading
+                ? dummyReportsModel.debts!
+                : state.reportsModel!.debts!,
+            branchCurrency: isLoading
+                ? dummyReportsModel.branchCurrency!
+                : state.reportsModel!.branchCurrency!,
+          ),
+        );
+      },
+    );
+  }
+}
+
+class DebtItemInReport extends StatelessWidget {
+  const DebtItemInReport({
+    super.key,
+    required this.model,
+    required this.branchCurrency,
+  });
+  final Debts model;
+  final BranchCurrency branchCurrency;
 
   @override
   Widget build(BuildContext context) {
@@ -29,58 +65,55 @@ class DebtsInReports extends StatelessWidget {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Text(
-           context.dailyDebt,
+            context.dailyDebt,
             style: context.textStyles.font16MediumSecondaryColor,
           ),
           14.verticalSizedBox,
           _buildGrayBox(
             context: context,
             title: context.internalDebt,
-            value: "12,500 د.ل",
+            value: "${model.inside?.balance.toString() ?? "0"} ${branchCurrency.code ?? ""}",
           ),
-            10.verticalSizedBox,
+          10.verticalSizedBox,
           _buildGrayBox(
             context: context,
             title: context.externalDebt,
-            value: "12,500 د.ل",
+            value: "${model.outside?.balance.toString() ?? "0"} ${branchCurrency.code ?? ""}",
           ),
         ],
       ),
     );
   }
 
-  Widget _buildGrayBox({required BuildContext context, required String title, required String value}) {
+  Widget _buildGrayBox({
+    required BuildContext context,
+    required String title,
+    required String value,
+  }) {
     return Container(
-          width: double.infinity,
-          padding: 12.allPadding,
-          decoration: BoxDecoration(
-            color: context.colors.backgroundFieldColor,
-            border: Border.all(
-              color: context.colors.strokeColor,
-              width: 1,
-            ),
-            borderRadius: BorderRadius.circular(8.r),
-          ),
-          child: Column(
+      width: double.infinity,
+      padding: 12.allPadding,
+      decoration: BoxDecoration(
+        color: context.colors.backgroundFieldColor,
+        border: Border.all(color: context.colors.strokeColor, width: 1),
+        borderRadius: BorderRadius.circular(8.r),
+      ),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    title,
-                    style: context.textStyles.font14MediumPrimaryColor.copyWith(
-                      color: context.colors.grayColor,
-                    ),
-                  ),
-                  Text(
-                    value,
-                    style: context.textStyles.font16MediumSecondaryColor
-                       
-                  ),
-                ],
+              Text(
+                title,
+                style: context.textStyles.font14MediumPrimaryColor.copyWith(
+                  color: context.colors.grayColor,
+                ),
               ),
+              Text(value, style: context.textStyles.font16MediumSecondaryColor),
             ],
           ),
-        );
+        ],
+      ),
+    );
   }
 }
