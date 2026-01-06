0e07270d0adedcfc1c3e0b5108528b201f8d074b
Salsabil <Salsabilelshiekh6@gmail.com>
2026-01-06 10:43:57 +0200

    feat: Enhance TransactionsDetailsView with loading indicator and update transaction handling

--- lib/features/transactions/presentation/views/transactions_details_view.dart.dart
+++ lib/features/transactions/presentation/views/transactions_details_view.dart.dart
@@ -1,3 +1,4 @@
+import 'package:alalamia/core/components/widgets/custom_modal_hub.dart';
 import 'package:alalamia/core/components/widgets/main_button.dart';
 import 'package:alalamia/core/enums/request_status.dart';
 import 'package:alalamia/core/enums/update_transaction_state_enum.dart';
@@ -16,6 +17,7 @@ import 'package:url_launcher/url_launcher.dart';
 import '../../../../core/components/widgets/custom_app_bar.dart';
 import '../../../../core/di/dependency_injection.dart';
 import '../../../../core/enums/status_enum.dart';
+import '../../../../core/enums/transactions_enum.dart';
 import '../../../../core/general/cubit/general_cubit.dart';
 import '../../../../core/utils/global_ui_utils.dart';
 import '../../../../generated/app_assets.dart';
@@ -52,121 +54,162 @@ class _TransactionsDetailsViewState extends State<TransactionsDetailsView> {
     }
   }
 
-  void _onDenominationsConfirmed(List<DenominationsRequestParams> denominations) {
-    final UpdateTransactionRequestParams updateTransactionRequestParams = UpdateTransactionRequestParams(
-      status: UpdateTransactionStatusEnum.recieved,
-      denominations: denominations,
-    );
+  void _onDenominationsConfirmed(
+    List<DenominationsRequestParams> denominations,
+  ) {
+    final UpdateTransactionRequestParams updateTransactionRequestParams =
+        UpdateTransactionRequestParams(
+          status: UpdateTransactionStatusEnum.recieved,
+          denominations: denominations,
+        );
     context.read<TransactionsCubit>().updateTransactionStatus(
       transactionId: widget.id,
       params: updateTransactionRequestParams,
     );
   }
 
-
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-        children: [
-          Image.asset(
-            AppAssets.imagesBackground,
-            width: double.infinity,
-            height: double.infinity,
-            fit: BoxFit.cover,
-          ),
-          SingleChildScrollView(
-            child: BlocBuilder<TransactionsCubit, TransactionsState>(
-              builder: (context, state) {
-                return Column(
-                  children: [
-                    CustomAppBar(
-                      title: context.transactions,
-                      hasActions: false,
-                      isBack: true,
-                    ).onlyPadding(bottomPadding: 24),
+    return CustomModelProgressIndecator(
+      inAsyncCall: context
+          .watch<TransactionsCubit>()
+          .state
+          .updateTransactionRequestStatus
+          .isLoading,
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Image.asset(
+              AppAssets.imagesBackground,
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+            ),
+            SingleChildScrollView(
+              child: BlocBuilder<TransactionsCubit, TransactionsState>(
+                builder: (context, state) {
+                  return Column(
+                    children: [
+                      CustomAppBar(
+                        title: context.transactions,
+                        hasActions: false,
+                        isBack: true,
+                      ).onlyPadding(bottomPadding: 24),
 
-                    Skeletonizer(
-                      enabled:
-                          state.transactionDetailsStatus ==
-                          RequestStatus.loading,
-                      child: Stack(
-                        clipBehavior: Clip.none,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(top: 25),
-                            padding: EdgeInsets.symmetric(
-                              horizontal: 16,
-                              vertical: 35,
-                            ),
-                            width: double.infinity,
-                            constraints: BoxConstraints(
-                              minHeight:
-                                  MediaQuery.of(context).size.height - 130.h,
-                            ),
-                            decoration: BoxDecoration(
-                              color: context.colors.whiteColor,
-                              borderRadius: BorderRadius.only(
-                                topLeft: Radius.circular(24),
-                                topRight: Radius.circular(24),
+                      Skeletonizer(
+                        enabled:
+                            state.transactionDetailsStatus ==
+                            RequestStatus.loading,
+                        child: Stack(
+                          clipBehavior: Clip.none,
+                          children: [
+                            Container(
+                              margin: EdgeInsets.only(top: 25),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 16,
+                                vertical: 35,
+                              ),
+                              width: double.infinity,
+                              constraints: BoxConstraints(
+                                minHeight:
+                                    MediaQuery.of(context).size.height - 130.h,
+                              ),
+                              decoration: BoxDecoration(
+                                color: context.colors.whiteColor,
+                                borderRadius: BorderRadius.only(
+                                  topLeft: Radius.circular(24),
+                                  topRight: Radius.circular(24),
+                                ),
+                              ),
+                              child: Column(
+                                children: [
+                                  25.verticalSizedBox,
+                                  TransferredAmountInfoWidget(),
+                                  20.verticalSizedBox,
+                                  SenderInfoCard(),
+                                  20.verticalSizedBox,
+                                  BeneficiaryInfoCard(),
+                                  20.verticalSizedBox,
+                                  TransactionInfoCard(),
+                                  32.verticalSizedBox,
+                                  context
+                                                  .read<TransactionsCubit>()
+                                                  .state
+                                                  .transactionDetails
+                                                  ?.recievingBranch ==
+                                              true &&
+                                          context
+                                                  .read<TransactionsCubit>()
+                                                  .state
+                                                  .transactionDetails
+                                                  ?.details
+                                                  .status ==
+                                              StatusEnum.pending
+                                      ? recivingButton()
+                                      : MainButton(
+                                          title: context.printReceipt,
+                                          onTap: () => _launchUrl(
+                                            state.transactionDetails?.pdfUrl ??
+                                                '',
+                                          ),
+                                        ),
+                                ],
                               ),
                             ),
-                            child: Column(
-                              children: [
-                                25.verticalSizedBox,
-                                TransferredAmountInfoWidget(),
-                                20.verticalSizedBox,
-                                SenderInfoCard(),
-                                20.verticalSizedBox,
-                                BeneficiaryInfoCard(),
-                                20.verticalSizedBox,
-                                TransactionInfoCard(),
-                                32.verticalSizedBox,
-                       context.read<TransactionsCubit>().state.transactionDetails?.recievingBranch == true && context.read<TransactionsCubit>().state.transactionDetails?.details.status == StatusEnum.pending ? recivingButton() : MainButton(
-                                  title: context.printReceipt,
-                                  onTap: () => _launchUrl(
-                                    state.transactionDetails?.pdfUrl ?? '',
-                                  ),
-                                ),
-                              ],
+                            PositionedDirectional(
+                              top: -10,
+                              start: 16,
+                              end: 16,
+                              child: StatusBox(
+                                status:
+                                    state.transactionDetails?.details.status ??
+                                    StatusEnum.pending,
+                              ),
                             ),
-                          ),
-                          PositionedDirectional(
-                            top: -10,
-                            start: 16,  
-                            end: 16,
-                            child: StatusBox(
-                              status:
-                                  state.transactionDetails?.details.status ??
-                                  StatusEnum.pending,
-                            ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                  ],
-                );
-              },
+                    ],
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
 
   Widget recivingButton() {
-    return MainButton(title: context.receivedDone, onTap: (){
-     GlobalUiUtils.showBottomSheet(
-        context,
-        child: BlocProvider.value(
-          value: getIt<GeneralCubit>(),
-          child: AllDenominationsBottomSheet(
-            amount: int.parse(context.read<TransactionsCubit>().state.transactionDetails?.amountSent ?? '0') ,
-            onConfirm: _onDenominationsConfirmed,
-          ),
-        ),
-      );
-    });
+    return BlocListener<TransactionsCubit, TransactionsState>(
+      listener: (context, state) {
+        if (state.updateTransactionRequestStatus.isSuccess) {
+          context.pop();
+          context.read<TransactionsCubit>().getTransactionList(transaction: TransactionsEnum.recieving);
+        }
+      },
+      child: MainButton(
+        title: context.receivedDone,
+        onTap: () {
+          GlobalUiUtils.showBottomSheet(
+            context,
+            child: BlocProvider.value(
+              value: getIt<GeneralCubit>(),
+              child: AllDenominationsBottomSheet(
+                amount: double.parse(
+                  context
+                          .read<TransactionsCubit>()
+                          .state
+                          .transactionDetails
+                          ?.amountSent ??
+                      '0',
+                ).toInt(),
+                onConfirm: _onDenominationsConfirmed,
+              ),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
